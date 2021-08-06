6afcf698bfc366b7a76611c168049ff5072ea850
Matheus Fornari <matheusfornari@hotmail.com>
2021-08-06 18:29:19 -0300

    50% projeto concluído

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos:[
+        {
+            numero: '45999',
+            nome: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+            {url:'45999.png', legenda: 'Vereador'}
+            ]
+
+        },
+        {
+            numero: '85555',
+            name: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos:[
+            {url:'85555.png', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+        {
+            numero: '10',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos:[
+            {url:'10.png', legenda: 'Prefeito'},
+            {url:'10_2.png', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '12',
+            nome: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos:[
+            {url:'12.png', legenda: 'Prefeito'},
+            {url:'12_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
